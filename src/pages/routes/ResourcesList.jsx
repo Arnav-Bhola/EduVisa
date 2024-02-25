09059e54410ddeff830aba67fe18f05857691b81
Atila Costa Almeida <chaves_87@hotmail.com>
2024-02-25 03:37:39 -0300

    feat: ResourcesSat added, styling not finished, and need check if structure is right

--- src/pages/routes/ResourcesList.jsx
+++ src/pages/routes/ResourcesList.jsx
@@ -1,10 +1,23 @@
-// import { useParams } from "react-router-dom"    LEAVE THIS CODE!
+import { useParams } from "react-router-dom";
 import styles from "./ResourcesList.module.scss";
+import ResourcesSat from "../../components/Resources/ResourcesSat";
 
 const ResourcesList = () => {
-  // const { id } = useParams();    LEAVE THIS CODE!
+  const { id } = useParams();
 
-  return <div className={styles["container"]}>{/*  */}</div>;
+  return (
+    <div className={styles["container"]}>
+      <header className={styles["header"]}>
+        <h1 className={styles["heading"]}>SAT Resources</h1>
+        <h2 className={styles["subheading"]}>
+          Explore the SAT resources created by our community!
+        </h2>
+      </header>
+      {id === "sat" && (<ResourcesSat/>)}
+      {id === "apexams" && <p>Content for /resources/apexams</p>}
+      {id !== "sat" && id !== "orange" && <p>PageNotExists</p>}
+    </div>
+  );
 };
 
 export default ResourcesList;
